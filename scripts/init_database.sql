ae2ee2f97979680c92e1f0a2d2558bf841b77040
mahahas20022-code <mahahas20022@gmail.com>
2025-10-17 18:22:35 +0300

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,22 @@
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
